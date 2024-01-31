219adcda9da4b753e4dd02d4a5b842c0ff7ba73c
Codetraker <swarajbibhuprasad@gmail.com>
2024-02-01 02:24:04 +0530

    adding output box

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import FormOne from './components/Bill/FormOne';
 import H1 from './components/UI/H1';
+import OutputBox from './components/UI/OutputBox';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <H1>Add Order</H1>
       <FormOne />
       <H1>Orders Detail</H1>
+      <OutputBox />
     </div>
   );
 }
---
+++ src/components/UI/OutputBox.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './OutputBox.css';
+
+function OutputBox(props){
+    return(
+        <div className='container'>
+            <div className='outOne'>
+                <div className='hfour'><h4>Table-1</h4></div>
+            </div>
+            <div className='outTwo'>
+                <div className='hfour'><h4>Table-2</h4></div>
+            </div>
+            <div className='outThree'>
+                <div className='hfour'><h4>Table-3</h4></div>
+            </div>
+        </div>
+    );
+}
+
+export default OutputBox;

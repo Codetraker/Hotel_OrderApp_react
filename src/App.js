3bfd83af37dd1a5cee49570e5d383368fc2f35d5
Codetraker <swarajbibhuprasad@gmail.com>
2024-02-02 19:11:25 +0530

    some change

--- src/App.js
+++ src/App.js
@@ -6,24 +6,45 @@ import List from './components/Bill/List';
 import OutputBox from './components/UI/OutputBox';
 
 function App() {
-  const [userList, setUserList] = useState([]);
+  const [tableOne, setTableOne] = useState([]);
+  const [tableTwo, setTableTwo] = useState([]);
+  const [tableThree, setTableThree] = useState([]);
 
-  const addUserHandler = (uId,uPrice,uDish,uTable) =>{
-    setUserList((prev)=>{
-      return [...prev, {id:uId, price:uPrice, dish:uDish, table:uTable}];
-    });
-  }
+  const addUserHandler = (uId,uPrice,uDish,uTable) =>{ 
+    const newUser = {id:uId, price:uPrice, dish:uDish, table:uTable};
+
+    switch (uTable) {
+        case '1':
+        setTableOne((prev)=>[...prev, newUser]);
+        break;
+        case '2':
+        setTableTwo((prev)=>[...prev, newUser]);
+        break;
+        case '3':
+        setTableThree((prev)=>[...prev, newUser]);
+        break;
+    
+        default:
+        break;
+      }
+    };
 
   return (
     <div>
       <H1>Add Order</H1>
       <FormOne onAddUser={addUserHandler}/>
       <H1>Orders Detail</H1>
-      <OutputBox>
-        <List users={userList}/>
+      <OutputBox >
+        <List users={tableOne}/>
       </OutputBox>
+      {/* <OutputBox table='2'>
+        <List users={tableTwo}/>
+      </OutputBox>
+      <OutputBox table='3'>
+        <List users={tableThree}/>
+      </OutputBox> */}
     </div>
   );
-}
+};
 
 export default App;
--- src/components/UI/OutputBox.js
+++ src/components/UI/OutputBox.js
@@ -2,25 +2,27 @@ import React from 'react';
 import './OutputBox.css';
 
 function OutputBox(props){
+    console.log(props);
+    
     return(
         <div className='container'>
             <div className='outOne'>
                 <div className='hfour'>
                     <h4>Table-1</h4>
                 </div>
-                <div>{props.children}</div>
+                <div>{props.table=='1'&&props.children}</div>
             </div>
             <div className='outTwo'>
                 <div className='hfour'>
                     <h4>Table-2</h4>
                 </div>
-                <div>{props.children}</div>
+                <div>{props.table=='2'&&props.children}</div>
             </div>
             <div className='outThree'>
                 <div className='hfour'>
                     <h4>Table-3</h4>
                 </div>
-                <div>{props.children}</div>
+                <div>{props.table=='3'&&props.children}</div>
             </div>
         </div>
     );
